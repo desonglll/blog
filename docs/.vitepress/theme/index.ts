5e12bdc8003e3f17cb70ecc7f48aa2ece418b849
desonglll <lindesong666@163.com>
2024-12-25 22:05:59 +0800

    Update google analytics

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,11 +2,14 @@
 import type { Theme } from "vitepress";
 import DefaultTheme from "vitepress/theme";
 import Mention from "../components/Mention.vue";
-
+import googleAnalytics from "vitepress-plugin-google-analytics";
 export default {
   extends: DefaultTheme,
   enhanceApp({ app }) {
     // register your custom global components
     app.component("Mention", Mention);
+    googleAnalytics({
+      id: "G-KB2KLVFYQE", // Replace with your GoogleAnalytics ID, which should start with the 'G-'
+    });
   },
 } satisfies Theme;
